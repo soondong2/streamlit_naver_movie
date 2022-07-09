3ae9af1800c60628d3470308779d463090bf560b
soondong2 <100760303+soondong2@users.noreply.github.com>
2022-07-09 18:57:58 +0900

    Update 1_💻_Movie_Star_&_Review.py

--- pages/1_💻_Movie_Star_&_Review.py
+++ pages/1_💻_Movie_Star_&_Review.py
@@ -166,7 +166,6 @@ section('평점 및 리뷰')
 
 if select_movie_code in movie_list()['영화코드'].tolist():
     data_load_state = st.text('Loading data...')
-    st.dataframe(all_review(select_movie_code, page_no))
-    st.pregress()
+    st.progress(st.dataframe(all_review(select_movie_code, page_no)))
     data_load_state.text("")
     
